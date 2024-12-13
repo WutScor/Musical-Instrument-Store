aa7ae907690a58dd31c6c7759119f421cba23ba6
22120231 <22120231@student.hcmus.edu.vn>
2024-12-14 05:08:57 +0700

    upd user page

---
+++ frontend/src/components/main/home/user/user-info.js
@@ -0,0 +1,64 @@
+import { FaPhone, FaLocationDot } from "react-icons/fa6";
+
+const UserInfo = () => {
+    return(
+        <>
+            <div className="user-info mt-3">
+                <div className="container-w3">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div className="user-img position-relative">
+                                <div className="user-cover">
+                                    <img src="https://images.pexels.com/photos/26447918/pexels-photo-26447918/free-photo-of-cat-lying-next-to-guitar.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                                        className="w-100"/>
+                                </div>
+                                <div className="user-avt position-absolute">
+                                    <img src="https://i.pinimg.com/originals/95/4e/0f/954e0f2ab2e4f5ade11b494a479fbf18.jpg"
+                                        className="w-100" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-8">
+                            <div className="d-flex flex-column">
+                                <div className="d-flex align-items-center justify-content-center mt-5">
+                                    <h1 className="user-name">Full Name</h1>
+                                </div>
+                                <div className="d-flex flex-column">
+                                    <div className="row">
+                                        <div className="col-md-4">
+                                            <div className="d-flex align-items-center mt-5 justify-content-end">
+                                                <FaLocationDot/><h5 className="ms-1 my-0">Address:</h5>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-8">
+                                            <div className="d-flex align-items-center mt-5 ms-5">
+                                                <h5 className="fw-normal m-0">University of Science - Ho Chi Minh City National University</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="d-flex flex-column">
+                                    <div className="row">
+                                        <div className="col-md-4">
+                                            <div className="d-flex align-items-center mt-5 justify-content-end">
+                                                <FaPhone/><h5 className="ms-1 my-0">Phone number:</h5>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-8">
+                                            <div className="d-flex align-items-center mt-5 ms-5">
+                                                <h5 className="fw-normal m-0">0666999666</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default UserInfo;
--- frontend/src/pages/Client/user.js
+++ frontend/src/pages/Client/user.js
@@ -1,5 +1,6 @@
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import UserInfo from "../../components/main/home/user/user-info";
 
 
 const UserPage = () => {
@@ -8,7 +9,7 @@ const UserPage = () => {
             <div className="main d-flex justify-content-center align-items-center user-page">
                 <div className="container-w1">
                     <div className="d-flex align-items-center justify-content-center flex-column textZone">
-                        <h1 className="mt-5">THIS IS USER PAGE</h1>
+                        <UserInfo/>
                         <Link to={'/signin'} className="mt-4"><Button>Sign In</Button></Link>
                         <Link to={'/signup'} className="mt-4"><Button>Sign Up</Button></Link>
                     </div>
