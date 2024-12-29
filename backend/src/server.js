765b58dab3d72de195b925a370c34b1dfc2f5637
21120540 <buuquang102@gmail.com>
2024-12-29 15:53:51 +0700

    change to https

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,15 @@
 require("dotenv").config();
+const https = require("https");
+const fs = require("fs");
 const app = require("./app");
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const options = {
+  key: fs.readFileSync(__dirname + "/../certs/key.pem"),
+  cert: fs.readFileSync(__dirname + "/../certs/cert.pem"),
+};
+
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`Server is running on https://localhost:${PORT}`);
 });
