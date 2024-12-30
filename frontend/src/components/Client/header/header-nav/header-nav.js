c52d2d9773730104d1d5c45b23ddb504f1fd6b89
Wut_đz <118157095+WutScor@users.noreply.github.com>
2024-12-30 10:35:38 +0700

    Hoàn thành Search

--- frontend/src/components/Client/header/header-nav/header-nav.js
+++ frontend/src/components/Client/header/header-nav/header-nav.js
@@ -1,17 +1,31 @@
-import { Link } from "react-router-dom";
-
+import { Link, useLocation } from "react-router-dom";
 
 const HeaderNav = () => {
-    return(
-        <>
-            <div className="header-nav me-auto d-flex justify-content-between">
-                <Link to={'/'}>Home</Link>
-                <Link to={'/shop'}>Shop</Link>
-                <Link to={'/about'}>About</Link>
-                <Link to={'/contact'}>Contact</Link>
-            </div>
-        </>
-    )
-}
+  const location = useLocation(); // Lấy thông tin về URL hiện tại
+
+  // Hàm kiểm tra xem trang hiện tại có khớp với đường dẫn hay không
+  const getLinkStyle = (path) => {
+    return location.pathname === path
+      ? { backgroundColor: '#FFD2A5', borderRadius: '45px', padding: '5px 15px' }  // Áp dụng style khi trang khớp
+      : {padding: '5px 10px'};
+  };
+
+  return (
+    <div className="header-nav me-auto d-flex justify-content-between">
+      <Link to="/" style={getLinkStyle("/")}>
+        Home
+      </Link>
+      <Link to="/shop" style={getLinkStyle("/shop")}>
+        Shop
+      </Link>
+      <Link to="/about" style={getLinkStyle("/about")}>
+        About
+      </Link>
+      <Link to="/contact" style={getLinkStyle("/contact")}>
+        Contact
+      </Link>
+    </div>
+  );
+};
 
 export default HeaderNav;
